51399b2c1fcf65daef47bd82e1c103207a9eec42
karthickraghuram <karthickraghuram.k@gmail.com>
2020-06-22 15:20:32 +0530

    Update CalcTest.java

--- src/main/java/com/edureka/TestCalculatorJar/CalcTest.java
+++ src/main/java/com/edureka/TestCalculatorJar/CalcTest.java
@@ -16,50 +16,53 @@ public class CalcTest
 		
 	    WebDriver driver;
 	    
-	    String myURL = "http://35.188.0.176:9090/calculator/";
-	    FirefoxOptions options = new FirefoxOptions();
-        
-        //options.addArguments("--headless");
-	    options.setCapability("requireWindowFocus", true);
-        String mygecko="/home/edureka/Downloads/geckodriver";
-        
+	     String myURL = "http://35.202.27.28:9999/";//"http://35.192.9.104:9090/calculator/";
+            FirefoxOptions options = new FirefoxOptions();
+
+        options.addArguments("--headless");
+            options.setCapability("requireWindowFocus", true);
+        String mygecko="/home/rsa-key-20200525/Downloads/geckodriver";
+
         System.setProperty("webdriver.gecko.driver",mygecko);
-        
+
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
-        
+
         driver = new FirefoxDriver(options);
-        
+
         driver.manage().timeouts().implicitlyWait(100, TimeUnit.SECONDS);
 
-	    System.out.println("Opening " + myURL);
+            System.out.println("Opening " + myURL);
 
         driver.get(myURL);
-        
+
         Thread.sleep(5000);
-        
-	    String text = "Addition";
+
+            String text = "This is about page";// "Addition";
 
         String bodyText = driver.findElement(By.xpath("/html/body")).getText();
         System.out.println(bodyText);
-	    driver.findElement(By.xpath("/html/body/form/input[1]")).sendKeys("12");
-	    driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("38");
-        
-	    driver.findElement(By.name("r1")).click();
-	    
-	    driver.findElement(By.xpath("/html/body/form/input[3]")).click();
-	    Thread.sleep(5000);
-	    
-	    bodyText = driver.findElement(By.xpath("/html/body")).getText();
-	    
-	    System.out.println(bodyText);
-	    
-	    Assert.assertTrue(bodyText.contains(text), text + " not found!" );
-	    
-	    Assert.assertTrue(bodyText.contains("50"), "50 not found!" );
-	    
-	    Thread.sleep(5000);
-	    
+//          driver.findElement(By.xpath("/html/body/form/input[1]")).sendKeys("12");
+//          driver.findElement(By.xpath("/html/body/form/input[2]")).sendKeys("38");
+
+           // driver.findElement(By.name("About Us")).click();
+
+           driver.findElement(By.xpath("/html/body/div/header/nav/a[2]")).click();
+
+//          driver.findElement(By.xpath("/html/body/form/input[3]")).click();
+            Thread.sleep(5000);
+
+            bodyText = driver.findElement(By.xpath("/html/body")).getText();
+
+            System.out.println(bodyText);
+
+            Assert.assertTrue(bodyText.contains(text), text + " not found!" );
+
+//          Assert.assertTrue(bodyText.contains("50"), "50 not found!" );
+
+            Thread.sleep(5000);
+
         driver.quit();
+
 	}
 }
